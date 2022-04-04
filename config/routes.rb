6887b12d2638593965f0b6b100ae8ab5ac47c664
shungo11260 <shungotaniguchi1120@gmail.com>
2022-04-04 01:06:09 -0700

    feat: add api for to_dos

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+  scope '/api' do
+    get '/to_dos', to: 'to_dos#index', defaults: { format: :json }
+    get '/to_dos/:id', to: 'to_dos#show', defaults: { format: :json }
+  end
 end
