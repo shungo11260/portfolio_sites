45a3f96c8448792f867df5e48498a243b47cb39a
shungo11260 <shungotaniguchi1120@gmail.com>
2022-04-04 01:08:24 -0700

    feat: limit  to_do action for only authenticated users

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,5 +1,6 @@
 class ToDosController < ApplicationController
   before_action :set_to_do, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: [:show, :new, :edit, :create, :update, :destroy]
 
   # GET /to_dos or /to_dos.json
   def index
