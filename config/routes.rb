798acf667313a18820d2d52d7768d0b8343afdb0
shungo11260 <shungotaniguchi1120@gmail.com>
2022-04-06 00:26:44 -0700

    feat: add list controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   resources :tasks
+  resources :lists
+
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
   scope '/api' do
     get '/tasks', to: 'tasks#index', defaults: { format: :json }
     get '/tasks/:id', to: 'tasks#show', defaults: { format: :json }
+    get '/lists', to: 'lists#index', defaults: { format: :json }
+    get '/lists/:id', to: 'lists#show', defaults: { format: :json }
   end
 end
